ff94da658d6adebb0d46700a9ffbc9beda43b8f0
FRANCIS-ANIMUTHU <104548375+FRANCIS-ANIMUTHU@users.noreply.github.com>
2022-08-13 20:44:49 +0530

    Add files via upload

---
+++ forloop/beginning.js
@@ -0,0 +1,48 @@
+//write a program table
+var a=20;
+var m=0;
+for(i=1;i<=20;i++){
+    console.log('a=',i*a);
+}
+
+//write a program integer to words
+let d;
+d=9;
+console.log('d=',d);
+for(i=1;i<=d;i++){
+    console.log('one');
+}
+for(i=2;i<=d;i++){
+    console.log('two');
+}
+for(i=3;i<=d;i++){
+    console.log('three');
+}
+for(i=4;i<=d;i++){
+        console.log('four');
+}
+for(i=5;i<=d;i++){
+        console.log('five');
+}
+for(i=6;i<=d;i++){
+        console.log('six');
+}
+for(i=7;i<=d;i++){
+        console.log('seven');
+}
+for(i=8;i<=d;i++){
+        console.log('eight');
+}
+for(i=9;i<=d;i++){
+        console.log('nine');
+}
+
+//write a program series
+var range=5;
+for(var i=1;i<=range;i++){
+    for(j=1;j<=i;j++){
+        console.log(i);
+        console.log('\t');
+        }
+}
+ 
